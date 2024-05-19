2e56d44db2b22e148b46b7f5ef71bec91f53c296
Jair Silva <119003861+silva808@users.noreply.github.com>
2024-05-19 13:45:53 -0500

    search in add_cita working

--- Php/new_preagendamiento.php
+++ Php/new_preagendamiento.php
@@ -1,3 +1,49 @@
+<<<<<<< HEAD
+<?php
+require_once 'conexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // JSON data
+    $null=" ";
+    $valorant='0';
+    $cita_idNumber = $data['cita_idNumber'];
+    // $cita_docType = $data['cita_docType'];
+    // $cita_name = $data['cita_name'];
+    // $cita_age = $data['cita_age'];
+    // $cita_afiliation = $data['cita_afiliation'];
+    $cita_typeCita = $data['cita_typeCita'];
+    $cita_date = $data['cita_date'];
+    $cita_startTime = $data['cita_startTime'];
+    $cita_endTime = $data['cita_endTime'];
+    $cita_sendTime = $data['cita_sendTime'];
+    // You can extract more fields as needed
+    
+    
+    $kali = mysqli_prepare($conn, "INSERT INTO preagendamiento (id_usuario, fecha, hora_inicio, valoracion, hora_fin, registro, id_tipo_cita) VALUES (?,?,?,?,?,?,?)");
+    // $kali2 = mysqli_prepare($conn, "INSERT INTO preagendamiento (id_usuario, fecha, hora_inicio, hora_fin, registro, id_tipo_cita) VALUES (?,?,?,?,?,?)");
+
+    mysqli_stmt_bind_param($kali, "ississi", $cita_idNumber, $cita_date, $cita_startTime, $valorant, $cita_endTime, $cita_sendTime, $cita_typeCita);
+
+    
+    
+    if (mysqli_stmt_execute($kali)) {
+        
+        echo "New CITA added successfully";
+    } else {
+        
+        echo "Error adding new CITA: " . mysqli_error($conn);
+    }
+
+    
+    mysqli_stmt_close($kali);
+    mysqli_close($conn);
+}
+?>
+=======
 <?php
 require_once 'conexion.php';
 
@@ -43,3 +89,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     mysqli_close($conn);
 }
 ?>
+>>>>>>> d71bce68f597541ba9042ab1eae4bf9d91869726
