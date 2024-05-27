1f7dfc7f0efcbef304d91ee1301ef806d2ada9fe
Jair Silva <jair.monteria.401@gmail.com>
2024-05-27 06:33:36 -0500

    lmao

--- Php/update_especialidad.php
+++ Php/update_especialidad.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- --------------------------------UPDATE USERS------------------------------------ -->
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -39,46 +38,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     mysqli_close($conn);
 }
 
-=======
-<!-- --------------------------------UPDATE USERS------------------------------------ -->
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
-    require_once 'conexion.php';
-
-    // Decode JSON data sent via AJAX
-    $data = json_decode(file_get_contents("php://input"), true);
-
-    // Prepare SQL statement
-    $jjk = "UPDATE especialidades SET 
-                especialidad = ?
-                WHERE id_especialidad = ?";
-
-    $stmtspecial = mysqli_prepare($conn, $jjk);
-
-    // Check if the prepare statement was successful
-    if ($stmtspecial) {
-        // Bind parameters
-        mysqli_stmt_bind_param(
-            $stmtspecial,
-            "si",
-            $data['ms_name'],
-            $data['ms_id']
-        );
-
-        // Execute statement
-        mysqli_stmt_execute($stmtspecial);
-
-        // Close statement
-        mysqli_stmt_close($stmtspecial);
-    } else {
-        // If prepare statement failed, handle the error
-        echo "Error: " . mysqli_error($conn);
-    }
-
-    // Close connection
-    mysqli_close($conn);
-}
-
->>>>>>> d71bce68f597541ba9042ab1eae4bf9d91869726
 ?>
